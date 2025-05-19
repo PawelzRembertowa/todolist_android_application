60fa44b6ee077185680ea0f20798628affbd01df
pawelm <dragon80@wp.pl>
2025-05-19 16:19:02 +0200

    WIP - styling not finished yet

--- app/src/main/java/com/example/todolistapplication/MainActivity.java
+++ app/src/main/java/com/example/todolistapplication/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements ToDoListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
